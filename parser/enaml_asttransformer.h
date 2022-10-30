5cfc382e3502b7af3f2e5f8af3a67e6179486968
frmdstryr <frmdstryr@protonmail.com>
2022-10-30 11:31:21 -0400

    Fix a few crashes and attempt fixing ranges

--- parser/enaml_asttransformer.h
+++ parser/enaml_asttransformer.h
@@ -2,6 +2,7 @@
 
 #include "python_asttransformer.h"
 #include "enaml_grammar.h"
+#include "enaml_ast.h"
 
 namespace Enaml {
 
@@ -13,11 +14,15 @@ public:
     QList<Python::Ast*> visitPythonModuleNode(PyObject* node, Python::Ast* parent);
     QList<Python::Ast*> visitEnamlDefBody(PyObject* node, Python::Ast* parent);
     Python::Ast* visitEnamlDefNode(PyObject* node, Python::Ast* parent);
-    Python::Ast* visitEnamlDefItemNode(PyObject* node, Python::Ast* parent);
+    QList<Python::Ast*> visitEnamlDefItemNode(PyObject* node, Python::Ast* parent);
     Python::Ast* visitTemplateNode(PyObject* node, Python::Ast* parent);
     Python::Ast* visitPythonExpressionNode(PyObject* node, Python::Ast* parent);
+    Python::Ast* visitPythonExpressionBlockNode(PyObject* node, Python::Ast* parent);
 
 private:
+    // Eg build a self.attr
+    Python::AttributeAst* newSelfAttr(int lineno, Python::Ast* parent, QString attr);
+
     Grammar enaml;
 };
 
