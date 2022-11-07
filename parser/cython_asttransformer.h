6f8255f208a582e0b60b370f17229e49b20a3df7
frmdstryr <frmdstryr@protonmail.com>
2022-11-07 13:15:23 -0500

    Disable cython support (not working) and unset def in enaml grammar

--- parser/cython_asttransformer.h
+++ parser/cython_asttransformer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "python_asttransformer.h"
+#include "cython_grammar.h"
 
 namespace Cython {
 
@@ -8,7 +9,12 @@ class AstTransformer: public Python::AstTransformer {
 public:
     AstTransformer(): Python::AstTransformer() {}
 
+    void run(PyObject *syntaxtree, QString moduleName) override;
     Python::Ast* visitNode(PyObject* node, Python::Ast* parent) override;
+
+    std::pair<int, int> cythonNodePos(PyObject* node);
+private:
+    Grammar cython;
 };
 
 } // end namespace Python
